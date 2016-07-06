d8af1f17623044be59cac749d884330f789fbeac
Clayton Freitas <sotiumkaboom@hotmail.com>
2016-07-06 14:38:52 -0300

    New documentation. DMZ function created.

--- HTMLShield.js
+++ HTMLShield.js
@@ -4,14 +4,24 @@
 
 function HTMLShield(interval, wrapjQuery) {
     var self = this;
+    /***** INSTANTIANTION *******/
 
+    /// privates
     self.__initialized = false;
     self.__htmlViolatedFunction = null;
     self.__interval = 1000;
     self.__isChecking = false; //Defines whether the Shield should check the DOM. Initialized with INIT
+    self.__timer = null; // Timer to handle setTimeout
+    self.__DEBUG = null;
+    self.__originalDOMHTML = null;
+
+
+    if (interval != null && interval >= 10) {
+        self.__interval = interval;
+    }
 
     self.__wrapKnockOut = function () {
-         
+
 
     }
 
@@ -22,15 +32,114 @@ function HTMLShield(interval, wrapjQuery) {
         }).forEach(function (funcName) {
             var orig = $.fn[funcName];
             $.fn[funcName] = function () {
-                self.__isChecking = false;
+                self.pause();
                 //console.log(funcName + " jQuery method called");
-                var returnVar = orig.apply(this, arguments);
-                self.setGroundHTML();
-                self.__isChecking = true;
+                var returnVar = orig.apply(this, arguments); 
+                self.start();
                 return returnVar;
             }
         });
     }
+     
+
+
+
+    //receives a function delegate to trigger when the HTML is violated
+    self.onHTMLViolated = function (fn) {
+        if (fn != null) {
+            self.__htmlViolatedFunction = fn;
+        }
+    }
+
+    //Defines the Base HTML for DOM Violation Checking
+    self.setGroundHTML = function () {
+        self.__log("Ground HTML set.");
+        self.__originalDOMHTML = document.getElementsByTagName("html")[0].innerHTML;
+    }
+
+
+    //Validates if DOM was violated. Trigger event if DOM is being monitored
+    self.validateDOM = function () {
+        if (self.__isChecking) {
+            var comparingHTML = document.getElementsByTagName("html")[0].innerHTML;
+            if (!self.__isHTMLUnviolated(self.__originalDOMHTML, comparingHTML)) {
+                if (self.__htmlViolatedFunction != null) {
+                    self.__log("HTML was violated.");
+                    self.__htmlViolatedFunction(self.__originalDOMHTML, comparingHTML);
+                }
+            }
+
+            setTimeout(self.validateDOM, self.__interval);
+        }
+    }
+
+    // Checks if DOM is sound, aligned with the base-version
+    self.__isHTMLUnviolated = function (baseElement, comparedElement) {
+        var baseElementHTML = baseElement;
+        var comparedElementHTML = comparedElement;
+        return (baseElementHTML == comparedElementHTML);
+    }
+
+    //initializes the 
+    self.init = function (startMonitoring) {
+        if (!self.__initialized) {
+            self.setGroundHTML();
+            self.__initialized = true;
+            if (startMonitoring == null || startMonitoring == true) {
+                self.start();
+            }
+            self.__log("Shield initialized.");
+        }
+        return self;
+    }
+
+    self.start = function () {
+        if ((!self.__initialized)) {
+            throw Error("Shield is not initialized. Call init() before starting the monitoring.");
+        }
+        self.setGroundHTML(); // now the base is the current HTML
+        self.__isChecking = true;
+        self.__timer = setTimeout(self.validateDOM, self.__interval);
+        self.__log("Shield started.");
+        return self;
+    }
+    self.pause = function () {
+        self.__isChecking = false;
+        if (self.__timer) {
+            clearTimeout(self.__timer);
+            self.__timer = null;
+            self.__log("Shield paused.");
+        }
+        return self;
+    }
+    // Allows DOM changes without triggering Violations
+    self.demilitarizedChange = function (operation)
+    {
+        var wasRunning = self.__isChecking;
+        if (wasRunning) {
+            self.pause();
+        }
+        if (operation)
+        {
+            operation();
+        }
+        if (wasRunning) {
+            self.start();
+        }
+    }
+
+    self.dmz = self.demilitarizedChange;
+
+    self.__log = function(message)
+    {
+        if (self.__DEBUG)
+        {
+            console.log(message);
+        }
+    }
+
+    //startup calls
+
 
     if (jQuery && wrapjQuery != null && wrapjQuery == true) {
         self.__wrapjQuery();
@@ -41,54 +150,6 @@ function HTMLShield(interval, wrapjQuery) {
     //    self.__wrapKnockOut();
     //}
 
-    if (interval != null && interval >= 10) {
-        self.__interval = interval;
-    }
-
-    self.myOriginalDOMHTML = null;
-
-    self.onHTMLViolated = function (fn) {
-        if (fn != null) {
-            self.__htmlViolatedFunction = fn;
-        }
-    }
-
-    //Defines the Base HTML for DOM Violation Checking
-    self.setGroundHTML = function () {
-        self.myOriginalDOMHTML = document.getElementsByTagName("html")[0].innerHTML;
-    }
-
-
-    //Validates if DOM was violated. Trigger event if DOM is being monitored
-    self.validateDOM = function () {
-        if (self.__isChecking) {
-            var comparingHTML = document.getElementsByTagName("html")[0].innerHTML;
-            if (!self.IsHTMLUnviolated(self.myOriginalDOMHTML, comparingHTML)) {
-                if (self.__htmlViolatedFunction != null) {
-                    self.__htmlViolatedFunction(self.myOriginalDOMHTML, comparingHTML);
-                    self.setGroundHTML(); // now the base is the current HTML
-                }
-            }
-
-            setTimeout(self.validateDOM, self.__interval);
-        }
-    }
-
-    self.IsHTMLUnviolated = function (baseElement, comparedElement) {
-        var baseElementHTML = baseElement;
-        var comparedElementHTML = comparedElement;
-        return (baseElementHTML == comparedElementHTML);
-    }
-
-    self.init = function () {
-        if (!self.__initialized) {
-            self.setGroundHTML();
-            setTimeout(self.validateDOM, self.__interval);
-            self.__initialized = true;
-            self.__isChecking = true;
-        }
-        return self;
-    }
-
-    return self.init();
+    //should it auto INIT?
+    return self ;
 }
--- README.md
+++ README.md
@@ -5,12 +5,24 @@ If you want to protect your HTML against developers trying to change it, or if y
 ## Usage
 Reference HTMLShield.js.
 
-After document is loaded you should instantiate an HTMLShield: 
+After document is loaded you should instantiate an HTMLShield. You can also associate the shield to your document to access it from other places:
 
-    $(function () {  
-             var shield = new HTMLShield(10, true); 
-             shield.onHTMLViolated( 
-                 function (prev, after) { 
-                     location.reload(); //Reload document if HTML is violated 
-                 })  
-       }) 
+	    $(function () {
+
+            document.htmlShield = new HTMLShield().init();
+            document.htmlShield.onHTMLViolated(
+					function (prev, after) {
+						$("html").html(prev); // recreates the whole DOM using jQuery
+						//Do something if document is violated
+					}
+			);
+
+        });
+
+### Initialization
+
+Initialization must happen after instantiation. To instantiate a shield you should define:
+* DOM Monitoring interval (minimum is 10ms)
+* If jQUery should be wrapped so as every change it makes to the DOM does not violate the HTML
+
+        new HTMLShield( interval, wrapjQuery)
